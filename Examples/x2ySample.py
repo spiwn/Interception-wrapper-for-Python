2e2451fbf4e62ba978d03f1801fbdcb11eea2837
Ivan Zhivkov <ipZhivkov@gmail.com>
2012-02-22 11:14:37 +0200

    Made explanation of example a bit more detailed

--- Examples/x2ySample.py
+++ Examples/x2ySample.py
@@ -1,4 +1,11 @@
-# A 'safe' python implementation of the Interception (https://github.com/oblitum/Interception) example
+#
+#
+# A 'safe' python implementation of
+# the Interception ( https://github.com/oblitum/Interception )
+# example x2y ( https://github.com/oblitum/Interception/tree/master/samples/x2y )
+#
+# Once running, it will turn your 'X' key into an 'Y' key
+# Press Escape to terminate
 
 import changePriority # https://gist.github.com/1876666
 from interception import *
@@ -22,7 +29,7 @@ def testInterception():
             if keyStroke.code == SCANCODE_ESC:
                 break
 
-            # if 'x' is pressed - turn it to 'y'
+            # if 'x' is pressed/released - turn it to 'y'
             if keyStroke.code == SCANCODE_X: keyStroke.code = SCANCODE_Y
 
             #uncomment the next line to also turn 'y's in to 'x's
