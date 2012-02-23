12ba836546effbe04d12a9724400eb95e7c5a90a
Ivan Zhivkov <ipZhivkov@gmail.com>
2012-02-23 18:06:04 +0200

    Rewrote the axes sample to use the context manager

--- Examples/axesSample.py
+++ Examples/axesSample.py
@@ -15,34 +15,24 @@ changePriority.IncreasePriority( times = 2 )
 SCANCODE_ESC    = 0x01
 
 def testInterception():
-    global context
-    stroke = Stroke()
     keyStroke = KeyStroke()
     mouseStroke = MouseStroke()
-    context = create_context()
-    set_filter( context, is_keyboard, FILTER_KEY_DOWN | FILTER_KEY_UP )
-    set_filter( context, is_mouse, FILTER_MOUSE_MOVE )
-    device = wait( context )
-    while ( receive ( context, device, stroke,1)>0 ):
-        if is_keyboard( device ):
-            stroke2KeyStroke( stroke, dest = keyStroke )
-            if keyStroke.code == SCANCODE_ESC:
-                break
-            send( context, device, stroke, 1 )
-        elif is_mouse( device ):
-            stroke2MouseStroke( stroke, dest = mouseStroke )
-            if not ( mouseStroke.flags & MOUSE_MOVE_ABSOLUTE ):
-                mouseStroke.y *= -1
-            send( context, device, mouseStroke, 1)
-        else:
-            send( context, device, stroke, 1 )
-        device = wait( context )
-    destroy_context( context )
 
+    with Context() as context:
+        context.set_filter( is_keyboard, FILTER_KEY_DOWN | FILTER_KEY_UP )
+        context.set_filter( is_mouse, FILTER_MOUSE_MOVE )
+        while ( context.wait() > 0 ):
+            if is_keyboard( context.device ):
+                stroke2KeyStroke( context.stroke, dest = keyStroke )
+                if keyStroke.code == SCANCODE_ESC:
+                    break
+                context.send( context.device, stroke)
+            elif is_mouse( context.device ):
+                stroke2MouseStroke( context.stroke, dest = mouseStroke )
+                if not ( mouseStroke.flags & MOUSE_MOVE_ABSOLUTE ):
+                    mouseStroke.y *= -1
+                context.send( context.device, mouseStroke)
+            else:
+                send( context, device, stroke, 1 )
 if __name__=='__main__':
-    try:
-        testInterception()
-    except Exception as e:
-        print('An Exception occured\n',e)
-        destroy_context(context)
-        raise e
+    testInterception()
